260ff5a9af09ffe5aecdd1ed3918df4aa952e1c3
tva2000hn <vietanhfat@gmail.com>
2020-08-11 23:29:55 +0700

    hello

--- src/main/java/com/jaguth/spigotpluggin/helloworld/HelloWorldPluggin.java
+++ src/main/java/com/jaguth/spigotpluggin/helloworld/HelloWorldPluggin.java
@@ -3,7 +3,12 @@ package com.jaguth.spigotpluggin.helloworld;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.player.PlayerBedEnterEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
@@ -16,13 +21,46 @@ public class HelloWorldPluggin extends JavaPlugin implements Listener {
 
         Bukkit.broadcastMessage("AWS Manager Pluggin Started!");
 
+
         // Registers our echo command executor
         this.getCommand("echo").setExecutor(new EchoCommandExecutor());
     }
 
     @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent playerJoinEvent) {
+    public void onJoined(PlayerJoinEvent playerJoinEvent) {
         Bukkit.broadcastMessage(playerJoinEvent.getPlayer().getDisplayName() + " has joined!");
+        playerJoinEvent.getPlayer().sendMessage("hello " +playerJoinEvent.getPlayer().getDisplayName() );
+        playerJoinEvent.getPlayer().setMaxHealth(0.5);
+        playerJoinEvent.getPlayer().setHealthScale(0.5);
+        playerJoinEvent.getPlayer().setHealthScaled(true);
+
     }
+
+    @EventHandler
+    public void respawn(PlayerRespawnEvent playerRespawnEvent){
+        playerRespawnEvent.getPlayer().setMaxHealth(0.5);
+        playerRespawnEvent.getPlayer().setHealthScale(0.5);
+        playerRespawnEvent.getPlayer().setHealthScaled(true);
+
+    }
+
+    @EventHandler
+    public void phablock(BlockBreakEvent blockBreakEvent){
+        blockBreakEvent.getPlayer().sendMessage("You broke " + blockBreakEvent.getBlock().getType().name());
+        if(blockBreakEvent.getBlock().getType().name().equals("SUGAR_CANE_BLOCK")){
+            blockBreakEvent.setDropItems(false);
+//                for(int i=0;i<10;i++){
+//
+//                }
+            blockBreakEvent.getPlayer().getInventory().addItem(new ItemStack(blockBreakEvent.getBlock().getType()));
+        }
+        else{blockBreakEvent.getPlayer().sendMessage("12345");}
+    }
+
+    @EventHandler
+    public void dingu(PlayerBedEnterEvent playerBedEnterEvent){
+        playerBedEnterEvent.getPlayer().sendMessage("Ngu ngon nhe");
+    }
+
 
 }
